17ba4a1a8b42284b85b1d44e86156dbbaede65cb
Stevie Migan <stevie@radcub.net>
2022-01-04 18:03:08 +0000

    Tidy code for intro and instructions

--- run.py
+++ run.py
@@ -1,22 +1,22 @@
-import random
+from random import randint
 
-print("**********************************************************************")
-print("Hello! Welcome to Hide & Seek!")
-print("Hide & Seek is a logic-based game built entirely in Python.")
-print("You have 5 team members that are hidden randomly on your grid...")
-print("Can you find the other team's 5 players before they find yours?")
-print("KEY")
-print("x = Sorry, Nobody is hiding there!")
-print("o = Bingo! You found one of my team!")
-print("**********************************************************************")
+scores = {"computer": 0, "player": 0}
 
-player_name = input("Firstly, what is your name?: ")
-print("\n")
-print("Hi " + player_name + ", let's play Hide & Seek!")
-print("\n")
+print("""**********************************************************************
+Hello! Welcome to Hide & Seek!
+Hide & Seek is a logic-based game built entirely in Python.
+You have 5 team members that are hidden randomly on your grid...
+Can you find the other team's 5 players before they find yours?
+KEY
+x = Sorry, Nobody is hiding there!
+o = Bingo! You found one of my team!
+**********************************************************************""")
 
-print(player_name,"- Here's where your team are hiding!")
-print("\n")
+player_name = input("Firstly, what is your name?: \n")
+print("Hi " + player_name + ", let's play Hide & Seek!\n")
+print(player_name,"- Here's where your team are hiding!\n")
+
+
 def create_grid_p1():
     grid = []
     for row in range(5):
@@ -33,3 +33,4 @@ def create_grid_p2():
         print(" - " * 5)
 
 create_grid_p2()
+
