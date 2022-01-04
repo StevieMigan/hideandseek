dd8bf545bb608e283ce08d029fff246724c4d140
Stevie Migan <stevie@radcub.net>
2022-01-04 17:41:48 +0000

    Add key to instructions panel at start of program

--- run.py
+++ run.py
@@ -5,11 +5,16 @@ print("Hello! Welcome to Hide & Seek!")
 print("Hide & Seek is a logic-based game built entirely in Python.")
 print("You have 5 team members that are hidden randomly on your grid...")
 print("Can you find the other team's 5 players before they find yours?")
+print("KEY")
+print("x = Sorry, Nobody is hiding there!")
+print("o = Bingo! You found one of my team!")
 print("**********************************************************************")
+
 player_name = input("Firstly, what is your name?: ")
 print("\n")
 print("Hi " + player_name + ", let's play Hide & Seek!")
 print("\n")
+
 print(player_name,"- Here's where your team are hiding!")
 print("\n")
 def create_grid_p1():
