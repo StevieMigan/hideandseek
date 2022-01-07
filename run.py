b7952c081b45631c8f2cbf38f4c71dc50fb0cfc7
Stevie Migan <stevie@radcub.net>
2022-01-07 15:50:31 +0000

    Add logic to Player 1 board for finding hiders

--- run.py
+++ run.py
@@ -1,8 +1,5 @@
 from random import randint
 
-player1_score = []
-player2_score = []
-
 print("""**********************************************************************
 Hello! Welcome to Hide & Seek!
 Hide & Seek is a logic-based game built entirely in Python.
@@ -17,24 +14,22 @@ player_name = input("Firstly, what is your name?: \n")
 print("Hi " + player_name + ", let's play Hide & Seek!\n")
 print(player_name,"- Here's where your team are hiding!\n")
 
-print("   0  1  2  3  4  ")
+print("     0  1  2  3  4  ")
 
-grid = []
+choice = [21,22,23]
+
+coords = 0
 for x in range(5):
-    print(x, " - " * 5)
+    row = ""
+    for y in range(5):
+        marker = " - "
+        if coords in choice:
+            marker = " X "
+        row = row + marker
+        coords = coords + 1
+    print(x," ",row)
 
 
-print("\n")
-print("Player two - Where are they hiding? ;)")
-print("\n")
-
-print("   0  1  2  3  4  ")
-
-grid2 = []
-for i in range(5):
-    print(i, " - " * 5)
-  
-guess = input("First, please guess a row: ")
 
 
 
